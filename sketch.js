a7e664ca1a6992df841c5eb2913a3f740c58d9f8
ShaneHickson <shane.hickson05@gmail.com>
2025-03-08 13:23:27 -0800

    medium changes

--- sketch.js
+++ sketch.js
@@ -1,6 +1,20 @@
+let oldMouseX = 0
+let oldMouseY = 0
+let curMouseX = 0
+let curMouseY = 0
+
 function setup() {
     createCanvas(400, 400);
+    background(220);
+    strokeWeight(20)
+    fill("black")
   }
   function draw() {
-    background(220);
+    oldMouseX = curMouseX
+    oldMouseY = curMouseY
+    curMouseX = mouseX
+    curMouseY = mouseY
+    if(mouseIsPressed === true){
+        line(oldMouseX, oldMouseY, curMouseX, curMouseY)
+    }
   }
